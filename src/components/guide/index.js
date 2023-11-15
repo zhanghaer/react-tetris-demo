17683d4666ffd1241810af089127b39ea429ad36
KevinChow <zhangxikevin@foxmail.com>
2023-11-15 11:05:58 +0800

    build

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -4,7 +4,6 @@ import style from './index.less';
 import { transform, i18n, lan } from '../../unit/const';
 import { isMobile } from '../../unit';
 
-
 export default class Guide extends React.Component {
   constructor() {
     super();
@@ -17,8 +16,9 @@ export default class Guide extends React.Component {
     if (this.state.isMobile) {
       return;
     }
-    QRCode.toDataURL(location.href, { margin: 1 })
-        .then(dataUrl => this.setState({ QRCode: dataUrl }));
+    QRCode.toDataURL(location.href, { margin: 1 }).then((dataUrl) =>
+      this.setState({ QRCode: dataUrl })
+    );
   }
   shouldComponentUpdate(state) {
     if (state.QRCode === this.state.QRCode) {
@@ -28,9 +28,7 @@ export default class Guide extends React.Component {
   }
   render() {
     if (this.state.isMobile) {
-      return (
-        null
-      );
+      return null;
     }
     return (
       <div style={{ display: this.state.isMobile ? 'none' : 'block' }}>
@@ -39,24 +37,46 @@ export default class Guide extends React.Component {
             <em style={{ [transform]: 'translate(0,-3px) scale(1,2)' }} />
           </div>
           <div className={style.left}>
-            <em style={{ [transform]: 'translate(-7px,3px) rotate(-90deg) scale(1,2)' }} />
+            <em
+              style={{
+                [transform]: 'translate(-7px,3px) rotate(-90deg) scale(1,2)',
+              }}
+            />
           </div>
           <div className={style.down}>
-            <em style={{ [transform]: 'translate(0,9px) rotate(180deg) scale(1,2)' }} /></div>
+            <em
+              style={{
+                [transform]: 'translate(0,9px) rotate(180deg) scale(1,2)',
+              }}
+            />
+          </div>
           <div className={style.right}>
-            <em style={{ [transform]: 'translate(7px,3px)rotate(90deg) scale(1,2)' }} />
+            <em
+              style={{
+                [transform]: 'translate(7px,3px)rotate(90deg) scale(1,2)',
+              }}
+            />
           </div>
         </div>
         <div className={`${style.guide} ${style.left}`}>
           <p>
-            <a href="https://github.com/chvin/react-tetris" rel="noopener noreferrer" target="_blank" title={i18n.linkTitle[lan]}>{`${i18n.github[lan]}:`}</a><br />
+            <a
+              href="https://github.com/chvin/react-tetris"
+              rel="noopener noreferrer"
+              target="_blank"
+              title={i18n.linkTitle[lan]}
+            >{`${i18n.github[lan]}:`}</a>
+            <br />
             <iframe
               src="https://ghbtns.com/github-btn.html?user=chvin&repo=react-tetris&type=star&count=true"
               frameBorder="0"
               scrolling="0"
               width="170px"
               height="20px"
-              style={{ [transform]: 'scale(1.68)', [`${transform}Origin`]: 'center left' }}
+              style={{
+                [transform]: 'scale(1.68)',
+                [`${transform}Origin`]: 'center left',
+              }}
             />
             <br />
             <iframe
@@ -65,21 +85,20 @@ export default class Guide extends React.Component {
               scrolling="0"
               width="170px"
               height="20px"
-              style={{ [transform]: 'scale(1.68)', [`${transform}Origin`]: 'center left' }}
+              style={{
+                [transform]: 'scale(1.68)',
+                [`${transform}Origin`]: 'center left',
+              }}
             />
           </p>
           <div className={style.space}>SPACE</div>
         </div>
-        { this.state.QRCode !== '' ? (
+        {this.state.QRCode !== '' ? (
           <div className={`${style.guide} ${style.qr}`}>
-            <img
-              src={this.state.QRCode}
-              alt={i18n.QRCode[lan]}
-            />
+            <img src={this.state.QRCode} alt={i18n.QRCode[lan]} />
           </div>
-        ) : null }
+        ) : null}
       </div>
     );
   }
 }
-
